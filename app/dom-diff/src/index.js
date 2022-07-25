eaf3b4106b9d9e6462dd3f0cfe890a7bca012ec6
李元庆 <liyuanq@weipaitang.com>
2022-07-25 19:49:01 +0800

    feat: dom-diff

---
+++ app/dom-diff/src/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+// index.js
+
+// 首先引入对应的方法来创建虚拟DOM
+import { createElement, render, renderDom } from './element';
+
+let virtualDom = createElement('ul', { class: 'list' }, [
+  createElement('li', { class: 'item' }, ['周杰伦']),
+  createElement('li', { class: 'item' }, ['林俊杰']),
+  createElement('li', { class: 'item' }, ['王力宏'])
+]);
+
+console.log(virtualDom);
+
+let el = render(virtualDom); // 渲染虚拟DOM得到真实的DOM结构
+console.log(el);
+// 直接将DOM添加到页面内
+renderDom(el, document.getElementById('root'));
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
